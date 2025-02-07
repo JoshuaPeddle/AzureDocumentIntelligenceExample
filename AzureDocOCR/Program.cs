251f2b5246063a28fa2e7a40f77a3e89172afadb
Joshua Peddle <joshuapeddle128@gmail.com>
2025-02-07 13:47:52 -0330

    Warnings cleanup

--- AzureDocOCR/Program.cs
+++ AzureDocOCR/Program.cs
@@ -17,39 +17,43 @@ namespace InvoiceRecognitionSample
             //var image = BinaryData.FromBytes(File.ReadAllBytes("path/to/invoice.png"));
             //var request = new AnalyzeDocumentOptions("prebuilt-invoice", image);
 
-            // https://user-images.githubusercontent.com/19628355/251216717-7e465227-cad3-4a85-b505-9c4441b31967.png
-            //https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/invoice_sample.jpg
-            //https://learn.microsoft.com/en-us/azure/ai-services/document-intelligence/media/contoso-invoice.png
-            string formUrl = "https://user-images.githubusercontent.com/19628355/251216717-7e465227-cad3-4a85-b505-9c4441b31967.png";
+            Dictionary<int, string> imageMap = new Dictionary<int, string>
+            {
+                { 1, "https://user-images.githubusercontent.com/19628355/251216717-7e465227-cad3-4a85-b505-9c4441b31967.png" },
+                { 2, "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/invoice_sample.jpg" },
+                { 3, "https://learn.microsoft.com/en-us/azure/ai-services/document-intelligence/media/contoso-invoice.png" },
+            };
+
 
             var timer = Stopwatch.StartNew();
 
-            var request = new AnalyzeDocumentOptions("prebuilt-invoice", new Uri(formUrl));
+            var request = new AnalyzeDocumentOptions("prebuilt-invoice", new Uri(imageMap[1]));
+
             var result = await client.AnalyzeDocumentAsync(WaitUntil.Completed, request);
 
-            foreach (var invoice in result.Value.Documents)
+            foreach (var invoiceFields in result.Value.Documents.Select(invoice => invoice.Fields))
             {
                 // Print header fields.
-                PrintField(invoice.Fields, "VendorName", "Vendor Name", f => f.ValueString);
-                PrintField(invoice.Fields, "VendorAddress", "Vendor Address", f => f.Content);
-                PrintField(invoice.Fields, "VendorAddressRecipient", "Vendor Address Recipient", f => f.ValueString);
-                PrintField(invoice.Fields, "CustomerName", "Customer Name", f => f.ValueString);
-                PrintField(invoice.Fields, "CustomerId", "Customer Id", f => f.ValueString);
+                PrintField(invoiceFields, "VendorName", "Vendor Name", f => f.ValueString);
+                PrintField(invoiceFields, "VendorAddress", "Vendor Address", f => $"\n{{{f.Content}}}");
+                PrintField(invoiceFields, "VendorAddressRecipient", "Vendor Address Recipient", f => f.ValueString);
+                PrintField(invoiceFields, "CustomerName", "Customer Name", f => f.ValueString);
+                PrintField(invoiceFields, "CustomerId", "Customer Id", f => f.ValueString);
                 // Customer Address uses a slightly different format.
-                PrintField(invoice.Fields, "CustomerAddress", "Customer Address", f => $"\n{{{f.Content}}}");
-                PrintField(invoice.Fields, "CustomerAddressRecipient", "Customer Address Recipient", f => f.ValueString);
-                PrintField(invoice.Fields, "InvoiceId", "Invoice Id", f => f.ValueString);
-                PrintField(invoice.Fields, "InvoiceDate", "Invoice Date", f => f.ValueDate.ToString());
-                PrintField(invoice.Fields, "InvoiceTotal", "Invoice Total", f => f.ValueCurrency.Amount.ToString());
-                PrintField(invoice.Fields, "DueDate", "Due Date", f => f.ValueDate.ToString());
-                PrintField(invoice.Fields, "PurchaseOrder", "Purchase Order", f => f.ValueString);
-                PrintField(invoice.Fields, "BillingAddress", "Billing Address", f => f.Content);
-                PrintField(invoice.Fields, "BillingAddressRecipient", "Billing Address Recipient", f => f.ValueString);
-                PrintField(invoice.Fields, "ShippingAddress", "Shipping Address", f => $"\n{{{f.Content}}}");
-                PrintField(invoice.Fields, "ShippingAddressRecipient", "Shipping Address Recipient", f => f.ValueString);
+                PrintField(invoiceFields, "CustomerAddress", "Customer Address", f => $"\n{{{f.Content}}}");
+                PrintField(invoiceFields, "CustomerAddressRecipient", "Customer Address Recipient", f => f.ValueString);
+                PrintField(invoiceFields, "InvoiceId", "Invoice Id", f => f.ValueString);
+                PrintField(invoiceFields, "InvoiceDate", "Invoice Date", f => f.ValueDate.ToString());
+                PrintField(invoiceFields, "InvoiceTotal", "Invoice Total", f => f.ValueCurrency.Amount.ToString());
+                PrintField(invoiceFields, "DueDate", "Due Date", f => f.ValueDate.ToString());
+                PrintField(invoiceFields, "PurchaseOrder", "Purchase Order", f => f.ValueString);
+                PrintField(invoiceFields, "BillingAddress", "Billing Address", f => f.Content);
+                PrintField(invoiceFields, "BillingAddressRecipient", "Billing Address Recipient", f => f.ValueString);
+                PrintField(invoiceFields, "ShippingAddress", "Shipping Address", f => $"\n{{{f.Content}}}");
+                PrintField(invoiceFields, "ShippingAddressRecipient", "Shipping Address Recipient", f => f.ValueString);
 
                 // Process invoice items.
-                if (invoice.Fields.TryGetValue("Items", out DocumentField itemsField))
+                if (invoiceFields.TryGetValue("Items", out DocumentField itemsField))
                 {
                     Console.WriteLine("Invoice items:");
                     foreach (var itemField in itemsField.ValueList)
@@ -64,16 +68,16 @@ namespace InvoiceRecognitionSample
                 }
 
                 // Print summary fields.
-                PrintField(invoice.Fields, "SubTotal", "Subtotal", f => f.ValueCurrency.Amount.ToString());
-                PrintField(invoice.Fields, "TotalTax", "Total Tax", f => f.ValueCurrency.Amount.ToString());
-                PrintField(invoice.Fields, "PreviousUnpaidBalance", "Previous Unpaid Balance", f => f.ValueCurrency.Amount.ToString());
-                PrintField(invoice.Fields, "AmountDue", "Amount Due", f => f.ValueCurrency.Amount.ToString());
-                PrintField(invoice.Fields, "ServiceStartDate", "Service Start Date", f => f.ValueDate.ToString());
-                PrintField(invoice.Fields, "ServiceEndDate", "Service End Date", f => f.ValueDate.ToString());
-                PrintField(invoice.Fields, "ServiceAddress", "Service Address", f => $"\n{{{f.Content}}}");
-                PrintField(invoice.Fields, "ServiceAddressRecipient", "Service Address Recipient", f => f.ValueString);
-                PrintField(invoice.Fields, "RemittanceAddress", "Remittance Address", f => f.Content);
-                PrintField(invoice.Fields, "RemittanceAddressRecipient", "Remittance Address Recipient", f => f.ValueString);
+                PrintField(invoiceFields, "SubTotal", "Subtotal", f => f.ValueCurrency.Amount.ToString());
+                PrintField(invoiceFields, "TotalTax", "Total Tax", f => f.ValueCurrency.Amount.ToString());
+                PrintField(invoiceFields, "PreviousUnpaidBalance", "Previous Unpaid Balance", f => f.ValueCurrency.Amount.ToString());
+                PrintField(invoiceFields, "AmountDue", "Amount Due", f => f.ValueCurrency.Amount.ToString());
+                PrintField(invoiceFields, "ServiceStartDate", "Service Start Date", f => f.ValueDate.ToString());
+                PrintField(invoiceFields, "ServiceEndDate", "Service End Date", f => f.ValueDate.ToString());
+                PrintField(invoiceFields, "ServiceAddress", "Service Address", f => $"\n{{{f.Content}}}");
+                PrintField(invoiceFields, "ServiceAddressRecipient", "Service Address Recipient", f => f.ValueString);
+                PrintField(invoiceFields, "RemittanceAddress", "Remittance Address", f => f.Content);
+                PrintField(invoiceFields, "RemittanceAddressRecipient", "Remittance Address Recipient", f => f.ValueString);
             }
 
             timer.Stop();
